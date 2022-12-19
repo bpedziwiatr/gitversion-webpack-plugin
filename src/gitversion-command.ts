84219662dc9185bbcaa01d7cc6da67ba721e03ec
Marcin Grabski <mgrabski@sii.pl>
2022-12-20 00:38:13 +0100

    feat: implement use dotnet-gitversion

--- src/gitversion-command.ts
+++ src/gitversion-command.ts
@@ -2,69 +2,66 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 export interface GitVersionModel {
-    Major: number;
-    Minor: number;
-    Patch: number;
-    PreReleaseTag: string;
-    PreReleaseTagWithDash: string;
-    PreReleaseLabel: string;
-    PreReleaseLabelWithDash: string;
-    PreReleaseNumber: number;
-    WeightedPreReleaseNumber: number;
-    BuildMetaData?: any;
-    BuildMetaDataPadded: string;
-    FullBuildMetaData: string;
-    MajorMinorPatch: string;
-    SemVer: string;
-    LegacySemVer: string;
-    LegacySemVerPadded: string;
-    AssemblySemVer: string;
-    AssemblySemFileVer: string;
-    FullSemVer: string;
-    InformationalVersion: string;
-    BranchName: string;
-    EscapedBranchName: string;
-    Sha: string;
-    ShortSha: string;
-    NuGetVersionV2: string;
-    NuGetVersion: string;
-    NuGetPreReleaseTagV2: string;
-    NuGetPreReleaseTag: string;
-    VersionSourceSha: string;
-    CommitsSinceVersionSource: number;
-    CommitsSinceVersionSourcePadded: string;
-    UncommittedChanges: number;
-    CommitDate: string;
+  Major: number;
+  Minor: number;
+  Patch: number;
+  PreReleaseTag: string;
+  PreReleaseTagWithDash: string;
+  PreReleaseLabel: string;
+  PreReleaseLabelWithDash: string;
+  PreReleaseNumber: number;
+  WeightedPreReleaseNumber: number;
+  BuildMetaData?: any;
+  BuildMetaDataPadded: string;
+  FullBuildMetaData: string;
+  MajorMinorPatch: string;
+  SemVer: string;
+  LegacySemVer: string;
+  LegacySemVerPadded: string;
+  AssemblySemVer: string;
+  AssemblySemFileVer: string;
+  FullSemVer: string;
+  InformationalVersion: string;
+  BranchName: string;
+  EscapedBranchName: string;
+  Sha: string;
+  ShortSha: string;
+  NuGetVersionV2: string;
+  NuGetVersion: string;
+  NuGetPreReleaseTagV2: string;
+  NuGetPreReleaseTag: string;
+  VersionSourceSha: string;
+  CommitsSinceVersionSource: number;
+  CommitsSinceVersionSourcePadded: string;
+  UncommittedChanges: number;
+  CommitDate: string;
 }
 export class GitVersionCommand {
-
-    private useDotnet:boolean
-    constructor(useDotnet:boolean) {
-        this.useDotnet = useDotnet;
-        
+  private useDotnet: boolean;
+  constructor(useDotnet: boolean) {
+    this.useDotnet = useDotnet;
+  }
+  async getVersion(): Promise<GitVersionModel> {
+    if (this.useDotnet === true) {
+      return this.getVersionDotnet();
+    } else {
+      return this.getVersionExe();
     }
-    async getVersion():Promise<GitVersionModel> {
-        if(this.useDotnet === true){
-            return this.getVersionDotnet();
-        }
-        else{
-            return this.getVersionExe();
-        }
+  }
+  async getVersionExe(): Promise<GitVersionModel> {
+    const { stdout, stderr } = await exec('gitversion');
+    if (stderr === null) {
+      throw Error(stderr);
     }
-    async getVersionExe():Promise<GitVersionModel> {
-        const { stdout, stderr } = await exec('gitversion');
-        if(stderr === null){
-            throw Error(stderr)
-        }
-        const gitversion = JSON.parse(stdout);
-        return gitversion
+    const gitversion = JSON.parse(stdout);
+    return gitversion;
+  }
+  async getVersionDotnet(): Promise<GitVersionModel> {
+    const { stdout, stderr } = await exec('dotnet-gitversion');
+    if (stderr === null) {
+      throw Error(stderr);
     }
-    async getVersionDotnet():Promise<GitVersionModel> {
-        const { stdout, stderr } = await exec('dotnet-gitversion');
-        if(stderr === null){
-            throw Error(stderr)
-        }
-        const gitversion = JSON.parse(stdout);
-        return gitversion
-    }
+    const gitversion = JSON.parse(stdout);
+    return gitversion;
+  }
 }
--- src/gitversion.plugin.ts
+++ src/gitversion.plugin.ts
@@ -1,79 +1,82 @@
-import { Compilation, Compiler } from 'webpack'
-import { GitVersionCommand, GitVersionModel } from './gitversion-command'
+import { Compilation, Compiler } from 'webpack';
+import { GitVersionCommand, GitVersionModel } from './gitversion-command';
 
 interface GitVersionPluginOptions {
-  appendFile?: boolean
-  regex?: string
-  useDotnet?:boolean
+  appendFile?: boolean;
+  regex?: string;
+  useDotnet?: boolean;
 }
 
 export class GitVersionPlugin {
-    gitVersionCommand: GitVersionCommand
-    gitVersion: GitVersionModel | null 
-    regex:string = "\.js$|\.css$"
-    appendFile:boolean = false
-  
-    constructor(options: GitVersionPluginOptions = {}) {
-      if(options.appendFile !== undefined){
-        this.appendFile = options.appendFile;
-      }
-      if(options.regex !== undefined){
-        this.regex =options.regex;
-      }
-      this.gitVersion = null;
-      this.gitVersionCommand = new GitVersionCommand(false);      
+  gitVersionCommand: GitVersionCommand;
+  gitVersion: GitVersionModel | null;
+  regex: string = '.js$|.css$';
+  appendFile: boolean = false;
+
+  constructor(options: GitVersionPluginOptions = {}) {
+    if (options.appendFile !== undefined) {
+      this.appendFile = options.appendFile;
     }
-  
-    async apply(compiler: Compiler) {    
-      
-      this.gitVersion = await this.gitVersionCommand.getVersion();
-      compiler.hooks.emit.tap('GitVersionPlugin', compilation => {
-        this.applyGitVersionToAssets(compilation);
-      });
+    if (options.regex !== undefined) {
+      this.regex = options.regex;
     }
-    validateFile(fileName:string) {
-      const regex = new RegExp(this.regex, "g");
-      return regex.test(fileName);
-    }
-    applyGitVersionToAssets(compilation: Compilation) {
-      Object.keys(compilation.assets).forEach((fileName) => {
-        if (!this.validateFile(fileName)) {
-          return;
-        }
-        const appendText = `// gitversion - ${this.gitVersion?.InformationalVersion}`;
-        let fileContent = compilation.assets[fileName].source();
-        if(this.appendFile){
-          
-          fileContent = `${fileContent}\n${appendText}`;
-        }else{
-          fileContent = `${appendText}\n${fileContent}`;
-        }
-        
-        compilation.assets[fileName] = {
-          source: function() {
-            console.log('source')
-            return fileContent
-          },
-          size: function() {
-            console.log('size')
-            return fileContent ? fileContent.length : 0
-          },
-          buffer: function() {
-            console.log('buffer')
-            return Buffer.from(fileContent)
-          },
-          map: function() {
-            console.log('map')
-            return {}
-          },
-          sourceAndMap: function() {
-            console.log('sourceAndMap')
-            return { source: fileContent, map: {} }
-          },
-          updateHash: function() {
-            console.log('updateHash')
-          },      
-        };
-      });
+    this.gitVersion = null;
+
+    if (options.useDotnet !== undefined) {
+      this.gitVersionCommand = new GitVersionCommand(options.useDotnet);
+    } else {
+      this.gitVersionCommand = new GitVersionCommand(false);
     }
   }
+
+  async apply(compiler: Compiler) {
+    this.gitVersion = await this.gitVersionCommand.getVersion();
+    compiler.hooks.emit.tap('GitVersionPlugin', compilation => {
+      this.applyGitVersionToAssets(compilation);
+    });
+  }
+  validateFile(fileName: string) {
+    const regex = new RegExp(this.regex, 'g');
+    return regex.test(fileName);
+  }
+  applyGitVersionToAssets(compilation: Compilation) {
+    Object.keys(compilation.assets).forEach(fileName => {
+      if (!this.validateFile(fileName)) {
+        return;
+      }
+      const appendText = `// gitversion - ${this.gitVersion?.InformationalVersion}`;
+      let fileContent = compilation.assets[fileName].source();
+      if (this.appendFile) {
+        fileContent = `${fileContent}\n${appendText}`;
+      } else {
+        fileContent = `${appendText}\n${fileContent}`;
+      }
+
+      compilation.assets[fileName] = {
+        source: function () {
+          console.log('source');
+          return fileContent;
+        },
+        size: function () {
+          console.log('size');
+          return fileContent ? fileContent.length : 0;
+        },
+        buffer: function () {
+          console.log('buffer');
+          return Buffer.from(fileContent);
+        },
+        map: function () {
+          console.log('map');
+          return {};
+        },
+        sourceAndMap: function () {
+          console.log('sourceAndMap');
+          return { source: fileContent, map: {} };
+        },
+        updateHash: function () {
+          console.log('updateHash');
+        },
+      };
+    });
+  }
+}
